4c897ee40c954a45cfeea0d1a40b4f1461c9d9b7
Сергей <rudensergey@gmail.com>
2019-12-19 06:36:32 +0300

    new tast (data structure)

---
+++ deleteOccurancies.js
@@ -0,0 +1,41 @@
+// Enough is enough!
+
+// Alice and Bob were on a holiday. Both of them took many pictures of the
+// places they've been, and now they want to show Charlie their entire collection.
+// However, Charlie doesn't like this sessions, since the motive usually repeats.
+// He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will
+// only sit during the session if they show the same motive at most N times. Luckily,
+// Alice and Bob are able to encode the motive as a number. Can you help them to remove
+// numbers such that their list contains each number only up to N times, without changing
+// the order?
+
+// Task
+
+// Given a list lst and a number N, create a new list that contains each number of 
+// lst at most N times without reordering. For example if N = 2, and the input is 
+// [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would 
+// lead to 1 and 2 being in the result 3 times, and then take 3, which leads to 
+// [1,2,3,1,2,3].
+
+function deleteNth(arr, n) {
+    let counter = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i-1]) {
+            counter = counter + 1;
+            console.log('counter = ' + counter)
+
+            if (counter > n && arr[i+1] === undefined) {
+                arr.splice((i - counter), (counter - n + 1));
+                console.log('Deleted ' + counter + ' numbers with 1st function ' + arr[i])
+                counter = 1;
+            } else if (counter > n && arr[i] != arr[i+1]) {
+                arr.splice(((i - counter)+1), (counter - n));
+                console.log('Deleted ' + counter + ' numbers with 2nd function ' + arr[i])
+                counter = 1;
+            }
+        }
+        console.log(arr)
+    }
+}
+
+deleteNth([20, 20 ,37, 37, 37, 37, 13, 13, 24, 24, 24, 11, 11, 11, 11], 2)
