933f1ca7531c2c3efd18b18f7c41e7516635c263
rudensergey <rudensergey@gmail.com>
2020-03-19 23:19:03 +0300

    new tasts with regular expressions

---
+++ 6К IP validation.js
@@ -0,0 +1,5 @@
+https://www.codewars.com/kata/515decfd9dcfc23bb6000006/javascript
+
+let isValidIP = str => /^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){3}(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/.test(str)
+
+// here iss a duplicate - I'm working on that
