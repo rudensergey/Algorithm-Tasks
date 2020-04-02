08c4466c7ed9a090dcda9d0bb5fd8f9390fa3ea4
rudensergey <rudensergey@gmail.com>
2020-04-02 22:25:17 +0300

    New resolved algorithm task

---
+++ 4K Adding Big Numbers.js
@@ -0,0 +1,29 @@
+
+
+function add(a, b) {
+    let first = a.split("").reverse()
+    let second = b.split("").reverse()
+    
+    let i = 0;
+    let result = [];
+
+    while (first[i] || second[i]) {
+        if (first[i] || second[i]) {
+            if (!first[i]) first[i] = 0
+            if (!second[i]) second[i] = 0
+
+            let sum =  +first[i] + +second[i]
+
+            if (result[i]) sum++
+            if (sum >= 10) {
+                let splitted = sum.toString().split("")
+                result[i] = +splitted[1]
+                result[i + 1] = 1;
+            } else {
+                result[i] = sum;
+            }
+        }
+        i++
+    }
+    return result.reverse().join("")
+}
