aa89429429a02ea59e4c9e6ee4cd36ffd7d1aed9
Sergey Rudenko <srudenko@more.tv>
2021-11-30 19:53:27 +0100

    add selection sort

---
+++ Sedgewick/selectionSort.py
@@ -0,0 +1,14 @@
+arr = [66, 43, 33, 22, 98, 45, 27, 1, 4, 7]
+
+def selection_sort(arr):
+  n = len(arr)
+  for i in range(n):
+    max = i
+    for j in range(n):
+      if arr[max] < arr[j]: max = j
+      arr[i], arr[max] = arr[max], arr[i]
+
+
+print(arr)
+selection_sort(arr)
+print(arr)
